f27096d6e3535af4cd022106d6129cc073e66886
maleeratL <maleerat.l.123@gmail.com>
2020-01-28 19:12:49 +1300

    Education and work button

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function displayEdu() {
+  document.getElementById("educationBox").style.display = "block";
+  document.getElementById("workBox").style.display = "none";
+}
+
+function displayWork() {
+  document.getElementById("educationBox").style.display = "none";
+  document.getElementById("workBox").style.display = "block";
+}
